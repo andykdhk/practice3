9c1dc2d7f3dff51161f88898f1982fe10dd757bb
Andrew Kang <andykdhk@gmail.com>
2020-11-13 22:54:45 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,53 @@
+/*
+File: main.cpp
+Date: Nov 13, 2020
+Name: Andrew Kang
+Goal: 
+study:
+
+*/
+#pragma warning(disable: 4996)
+
+#include "practice1.h"
+#include<stdio.h>
+
+
+int main(void)
+{
+	int menu;
+	int retMenu;
+
+	while (1)
+	{
+		printf("[0]:EXIT \n");
+		printf("[1]: \n");
+
+		retMenu = scanf("%d", &menu);
+		if (retMenu == 0)
+		{
+			printf("ERROR: MENU SELECTION\n");
+			return 0;
+		}
+		fflush(stdin);
+	
+		switch (menu)
+		{
+			case 1:
+				
+				printf("PRACTICE 1 \n");
+				break;
+
+			case 0:
+
+				printf("BYE \n");
+				return 0;
+			
+			default:
+				printf("ERROR: selection does not exist \n");
+				break;
+		}
+	
+	}
+
+	return 0;
+}
---
+++ practice1.cpp
@@ -0,0 +1,11 @@
+/*
+File: practice1.cpp
+Date: Nov 13, 2020
+Name: Andrew Kang
+Goal:
+study:
+
+*/
+
+
+#include<stdio.h>
